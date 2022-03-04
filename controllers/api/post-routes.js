244a8a5f22948c5378a679057c0f0fe0d0ad81b6
Ben <benjamind10@pm.me>
2022-03-03 19:06:27 -0500

    added user routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -94,7 +94,7 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
